7f8236229d6df851dd580727d6a401036f64543b
PyoJunCode <seojun0602@gmail.com>
2023-02-05 23:23:06 +0900

    update: add vgg loss

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,8 +1,10 @@
-from losses.losses import ColorLoss, SpaExpLoss, SpatialLoss, IlluSmoothLoss
-
+from losses.losses import ColorLoss, SpaExpLoss, SpatialLoss, IlluSmoothLoss, L1Loss
+from losses.vgg_loss import PerceptualLoss
 __all__ = [
     "ColorLoss",
     "SpaExpLoss",
     "SpatialLoss",
-    "IlluSmoothLoss"
+    "IlluSmoothLoss",
+    "L1Loss",
+    "PerceptualLoss"
 ]
--- losses/losses.py
+++ losses/losses.py
@@ -53,9 +53,8 @@ class SpatialLoss(nn.Module):
         org_pool =  self.pool(org_mean)			
         enhance_pool = self.pool(enhance_mean)	
 
-        weight_diff =torch.max(torch.FloatTensor([1]).cuda() + 10000*torch.min(org_pool - torch.FloatTensor([0.3]).cuda(),torch.FloatTensor([0]).cuda()),torch.FloatTensor([0.5]).cuda())
-        E_1 = torch.mul(torch.sign(enhance_pool - torch.FloatTensor([0.5]).cuda()) ,enhance_pool-org_pool)
-
+        # weight_diff =torch.max(torch.FloatTensor([1]).cuda() + 10000*torch.min(org_pool - torch.FloatTensor([0.3]).cuda(),torch.FloatTensor([0]).cuda()),torch.FloatTensor([0.5]).cuda())
+        # E_1 = torch.mul(torch.sign(enhance_pool - torch.FloatTensor([0.5]).cuda()) ,enhance_pool-org_pool)
 
         D_org_letf = F.conv2d(org_pool , self.weight_left, padding=1)
         D_org_right = F.conv2d(org_pool , self.weight_right, padding=1)
@@ -86,18 +85,51 @@ class SpaExpLoss(nn.Module):
     def forward(self, x, pred):
 
         b,c,h,w = x.shape
+        # RGB mean
         x = torch.mean(x, 1, keepdim=True)
         pred = torch.mean(pred, 1, keepdim=True)
+        # Pooling
         mean_x = self.pool(x)
         mean_pred = self.pool(pred)
 
+        # Calculate Distance
         distance = torch.mean(torch.pow(mean_pred - mean_x, 2))
         return self.loss_weight * distance
 
 
 
+# class IlluSmoothLoss(nn.Module):
+    # def __init__(self, loss_weight=1):
+    #     super().__init__()
+    #     self.loss_weight = loss_weight
+
+    # def forward(self,x):
+    #     print(x.shape)
+    #     # batch_size = x.size()[0]
+    #     # h_x = x.size()[2]
+    #     # w_x = x.size()[3]
+    #     # count_h =  (x.size()[2]-1) * x.size()[3]
+    #     # count_w = x.size()[2] * (x.size()[3] - 1)
+    #     h_tv = (x[:,:,1:,:]-x[:,:,:-1,:])
+    #     w_tv = (x[:,:,:,1:]-x[:,:,:,:-1])
+
+    #     h_tv = h_tv.abs()
+    #     w_tv = w_tv.abs()
+
+    #     # h_tv = h_tv.sum()
+    #     # w_tv = w_tv.sum()
+
+    #     print(h_tv.shape, w_tv.shape)
+
+    #     l_tv = torch.pow((h_tv+w_tv), 2).sum()
+    #     l_tv = torch.mean(l_tv)
+
+    #     print(l_tv)
+        
+    #     return self.loss_weight * l_tv
+
 class IlluSmoothLoss(nn.Module):
-    def __init__(self, loss_weight=1):
+    def __init__(self,loss_weight=1):
         super().__init__()
         self.loss_weight = loss_weight
 
@@ -110,8 +142,10 @@ class IlluSmoothLoss(nn.Module):
         h_tv = torch.pow((x[:,:,1:,:]-x[:,:,:h_x-1,:]),2).sum()
         w_tv = torch.pow((x[:,:,:,1:]-x[:,:,:,:w_x-1]),2).sum()
 
-        return self.loss_weight*2*(h_tv/count_h+w_tv/count_w)/batch_size
-
+        #print(torch.mean(h_tv/count_h + w_tv/count_w))
+        #print((h_tv/count_h+w_tv/count_w)/batch_size)
+        return self.loss_weight * 2 * (h_tv/count_h+w_tv/count_w)/batch_size
+        #return self.loss_weight * torch.mean(h_tv/count_h + w_tv/count_w)
 
 # class IlluSmoothLoss(nn.Module):
 #     def __init__(self, loss_weight=1):
@@ -123,3 +157,32 @@ class IlluSmoothLoss(nn.Module):
 #         loss = torch.mean(loss)
 
 #         return self.loss_weight * loss
+
+class L1Loss(nn.Module):
+    """L1 (mean absolute error, MAE) loss.
+
+    Args:
+        loss_weight (float): Loss weight for L1 loss. Default: 1.0.
+        reduction (str): Specifies the reduction to apply to the output.
+            Supported choices are 'none' | 'mean' | 'sum'. Default: 'mean'.
+    """
+
+    def __init__(self, loss_weight=1.0, reduction='mean'):
+        super(L1Loss, self).__init__()
+        if reduction not in ['none', 'mean', 'sum']:
+            raise ValueError(f'Unsupported reduction mode: {reduction}. ' f'Supported ones are: {_reduction_modes}')
+
+        self.loss_weight = loss_weight
+        self.reduction = reduction
+
+        self.l1_loss = nn.L1Loss(reduction=reduction)
+
+    def forward(self, pred, target, weight=None, **kwargs):
+        """
+        Args:
+            pred (Tensor): of shape (N, C, H, W). Predicted tensor.
+            target (Tensor): of shape (N, C, H, W). Ground truth tensor.
+            weight (Tensor, optional): of shape (N, C, H, W). Element-wise
+                weights. Default: None.
+        """
+        return self.loss_weight * self.l1_loss(pred, target)
